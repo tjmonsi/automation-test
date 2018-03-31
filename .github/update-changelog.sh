619c3592caac8eedd57e76d8cd293880b620c403
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-03-31 15:20:07 +0800

    first commit
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

---
+++ .github/update-changelog.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+git checkout -b changelog_update
+github_changelog_generator
+git add CHANGELOG.md
+git commit -m "changelog update [ci skip]"
+git push -u origin changelog_update
+string=`git config --get remote.origin.url`
+prefix="git@github.com:"
+suffix=".git"
+repo=${string#$prefix}
+repo=${repo%$suffix}
+url=$(curl -v -d '{"title":"Changelog Update","head":"changelog_update","base":"master"}' -H "Content-Type: application/json" -H "Authorization: token ${CHANGELOG_GITHUB_TOKEN}" https://api.github.com/repos/${repo}/pulls | jq '.url')
+echo $url
+if [ -z ${url+x} ]; then
+  echo 'Error on creating pull request'
+else
+  url=${url#\"}
+  url=${url%\"}
+  sha=$(curl -v -H "Content-Type: application/json" -H "Authorization: token ${CHANGELOG_GITHUB_TOKEN}" -X PUT ${url}/merge | jq '.merged')
+  if [ -z ${sha+x} ]; then
+    https://api.github.com/repos/${repo}/pulls
+  else
+    curl -v -H "Content-Type: application/json" -H "Authorization: token ${CHANGELOG_GITHUB_TOKEN}" -X DELETE https://api.github.com/repos/${repo}/git/refs/heads/changelog_update
+  fi
+fi
